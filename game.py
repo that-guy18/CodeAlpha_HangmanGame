c95dff070fd19964c32f769ad372231ec9ae8492
that-guy18 <shivamkumarsk208@gmail.com>
2025-07-05 22:25:14 +0530

    game.py

--- game.py
+++ game.py
@@ -1,18 +1,7 @@
 import random
 
 # List of words to choose from
-word_list = [
-    "chess", "snacks", "player", "grape", "beach", "mango", "lemon", "gym", "laws", "plum",
-    "python", "coding", "program", "script", "function", "variable", "loop", "while", "input", "output",
-    "school", "college", "teacher", "student", "classroom", "exam", "study", "pencil", "notebook", "homework",
-    "planet", "galaxy", "universe", "asteroid", "comet", "rocket", "astronaut", "orbit", "moon", "earth",
-    "ocean", "island", "desert", "forest", "jungle", "valley", "mountain", "river", "volcano", "climate",
-    "football", "cricket", "tennis", "hockey", "basketball", "baseball", "golf", "running", "cycling", "swimming",
-    "puzzle", "riddle", "mystery", "detective", "crime", "thief", "clue", "suspect", "evidence", "investigate",
-    "lion", "tiger", "elephant", "zebra", "giraffe", "kangaroo", "panda", "dolphin", "whale", "eagle",
-    "music", "guitar", "piano", "violin", "drums", "singer", "melody", "lyrics", "concert", "album",
-    "winter", "spring", "summer", "autumn", "snow", "rain", "sunny", "cloudy", "storm", "wind"
-]
+word_list = ["rain", "sunny", "cloudy", "storm", "wind"]
 
 # Choose a random word
 chosen_word = random.choice(word_list)
